ce96e090351fc2985f42055aededd4c4efa9991a
Ahmet Yıldız <yildizahmet2009@gmail.com>
2022-12-10 20:57:15 +0300

    Update pose-estimation.py

--- pose-estimation.py
+++ pose-estimation.py
@@ -31,10 +31,6 @@ if __name__ == "__main__":
     cap = cv2.VideoCapture(0)
 
     fps = int(cap.get(cv2.CAP_PROP_FPS))
-    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    frame_size = (width, height)
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 
     while cap.isOpened():
         success, image = cap.read()
